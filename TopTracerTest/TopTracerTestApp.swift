72bd0581551475c678064ad4715a4944ed3c09fe
Tommy Malmström <tommy.malmstrom@live.se>
2022-04-20 15:08:10 +0200

    Initial Commit

---
+++ TopTracerTest/TopTracerTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  TopTracerTestApp.swift
+//  TopTracerTest
+//
+//  Created by Tommy on 2022-04-20.
+//
+
+import SwiftUI
+
+@main
+struct TopTracerTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
